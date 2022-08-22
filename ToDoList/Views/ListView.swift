b3eb2de5b73c9890a9c0c8466497bdecfc577f10
amtjitro <60487970+amtjitro@users.noreply.github.com>
2022-08-22 14:30:32 -0700

    Created item model which allows for better functionality of items on the to-do list

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items: [String] = [
-        "This is the first title!",
-        "This is the second!",
-        "Third!"
+    @State var items: [ItemModel] = [
+        ItemModel(title: "This is the first title", isCompleted: false),
+        ItemModel(title: "This is the second", isCompleted: true),
+        ItemModel(title: "Third!", isCompleted: false),
     ]
-    
     var body: some View {
         List {
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
+            ForEach(items) { item in
+                ListRowView(item: item)
             }
         }
         .listStyle(PlainListStyle())
         .navigationTitle("To-Do List 📝")
         .navigationBarItems(
             leading: EditButton(),
-            trailing: NavigationLink("Add", destination: Text("destination")))
+            trailing: NavigationLink("Add", destination: AddView())
+        )
     }
 }
 
