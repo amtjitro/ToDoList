7b6d028be06d84790f39cf963d6b0542b69026cc
amtjitro <60487970+amtjitro@users.noreply.github.com>
2022-08-22 11:57:39 -0700

    created the 'add view' page for adding a new task

---
+++ ToDoList/Views/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  ToDoList
+//
+//  Created by Alessandra Tjitro on 8/22/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here:", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(hue: 1.0, saturation: 0.022, brightness: 0.911))
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item ✍️")
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            AddView()
+        }
+        
+    }
+}
